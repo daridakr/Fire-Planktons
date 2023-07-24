afbdaa733585881bde82179615f1aadef7e308a0
Krivushkina Daria <daridakr@gmail.com>
2023-07-24 19:06:55 +0000

    Added particle destroy affect

--- Assets/Scripts/Burger/BurgerPiece.cs
+++ Assets/Scripts/Burger/BurgerPiece.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class BurgerPiece : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _destroyEffect;
+
     public event UnityAction<BurgerPiece> Hitted;
 
     public void Destroy()
     {
         Hitted?.Invoke(this);
+
+        ParticleSystemRenderer effectRenderer = Instantiate(_destroyEffect, transform.position, _destroyEffect.transform.rotation).GetComponent<ParticleSystemRenderer>();
+        effectRenderer.material.color = GetComponent<MeshRenderer>().material.color;
+
         Destroy(gameObject);
     }
 }
